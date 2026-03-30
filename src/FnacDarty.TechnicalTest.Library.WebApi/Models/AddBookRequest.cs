dd7f8c98f9fb57bdb2c7a29799f6ec203821cc5d
Oussama <kahouloussama@gmail.com>
2026-03-30 19:22:49 +0200

    Fix AddBook Error: Setters + Default Constructor

--- src/FnacDarty.TechnicalTest.Library.WebApi/Models/AddBookRequest.cs
+++ src/FnacDarty.TechnicalTest.Library.WebApi/Models/AddBookRequest.cs
@@ -2,14 +2,18 @@
 {
     public class AddBookRequest
     {
-        public string Title { get; }
+        public string Title { get; set; }
 
-        public string Author { get; }
+        public string Author { get; set; }
 
         public AddBookRequest(string title, string author)
         {
             Title = title;
             Author = author;
         }
+
+        public AddBookRequest() { }
+
+
     }
 }
